338105bc041b0c3ce8abb391f7d56dd9e89ad2ce
Matheus Antoniele <matheus.antoniele@gmail.com>
2026-06-12 11:07:42 -0300

    Criacao Banco

---
+++ Banco.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE biblioteca;
+USE biblioteca;
+
+CREATE TABLE livros (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    autor VARCHAR(255) NOT NULL,
+    ano_publicacao INT,
+    disponivel BOOLEAN DEFAULT TRUE
+);
