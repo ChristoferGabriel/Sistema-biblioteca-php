4c4e4c4d52ff01fbdb055ace5efff6474be2df18
Matheus Antoniele <matheus.antoniele@gmail.com>
2026-06-12 11:16:16 -0300

    Alteracao classe LoginController

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -1,34 +1,30 @@
 <?php
-
 session_start();
 
-require_once 'conexao.php';
+require_once 'config/conexao.php';
 
-$pdo = conectar();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    $pdo = conectar();
 
-$sql = $pdo->prepare(
-"SELECT * FROM caixa
-WHERE usuario=:usuario");
-
-    $sql->execute([
-    ':usuario'=>$_POST['usuario']
+    $sql = $pdo->prepare("SELECT * FROM caixa WHERE usuario = :usuario");
+        $sql->execute([
+        ':usuario' => $_POST['usuario']
     ]);
 
-    $caixa = $sql->fetch();
+    $caixa = $sql->fetch(PDO::FETCH_ASSOC);
 
-    if($caixa)
-    {
-    if(password_verify(
-        $_POST['senha'],
-        $caixa['senha']
-    ))
-    {
-        $_SESSION['caixa'] =
-        $caixa['usuario'];
-
-        header("Location:
-        ../views/menu.php");
+    if ($caixa) {
+        if (password_verify($_POST['senha'], $caixa['senha'])) {
+            
+            $_SESSION['caixa'] = $caixa['usuario'];
+            header("Location: ../views/menu.php");
+            exit; 
+        }
     }
-    }
+    echo "Login inválido";
 
-echo "Login inválido";
+} else {
+    header("Location: ../views/login.php");
+    exit;
+}
