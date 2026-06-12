77f926836bb633c68604f08d47b7ed366676ad2c
Matheus Antoniele <matheus.antoniele@gmail.com>
2026-06-12 11:12:57 -0300

    Correcao classe Login

--- view/Login.php
+++ view/Login.php
@@ -1,23 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
+    <meta charset="UTF-8">
     <title>Login</title>
 </head>
 <body>
     <h1>Login do Caixa</h1>
 
-    <form method="POST" action="LoginController.php">
+    <form method="POST" action="../controllers/LoginController.php">
 
-        <input type="email" name="email" placehobr><br>
+        <input type="text" name="usuario" placeholder="Usuário" required><br><br>
 
-        <input type="password" name="senha" placeholder="Senha" required><br>
+        <input type="password" name="senha" placeholder="Senha" required><br><br>
         
         <button type="submit">Entrar</button>
-
     </form>
-
 </body>
 </html>
-
-
-
