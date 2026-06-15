4bc9f14ae5f24dfcf47f0c9e52782a57af89a2a7
Christofer Gabriel <christofergabriel2007@gmail.com>
2026-06-15 15:16:22 -0300

    Alteração total Livro.php

--- models/Livro.php
+++ models/Livro.php
@@ -1,22 +1,80 @@
 <?php
-class Livro {
+
+require_once __DIR__ . '/../config/conexao.php';
+
+class Livro
+{
     private $pdo;
 
-    public function __construct($pdo) {
+    public function __construct($pdo)
+    {
         $this->pdo = $pdo;
     }
-    public function listar() {
-        $sql = "SELECT * FROM livros ORDER BY titulo ASC";
-        $stmt = $this->pdo->query($sql);
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-    public function cadastrar($titulo, $autor, $ano_publicacao) {
-        $sql = "INSERT INTO livros (titulo, autor, ano_publicacao) VALUES (:titulo, :autor, :ano_publicacao)";
+
+    public function cadastrar($titulo, $autor, $ano)
+    {
+        $sql = "INSERT INTO livros(titulo, autor, ano)
+                VALUES(:titulo, :autor, :ano)";
+
         $stmt = $this->pdo->prepare($sql);
+
         return $stmt->execute([
             ':titulo' => $titulo,
             ':autor' => $autor,
-            ':ano_publicacao' => $ano_publicacao
+            ':ano' => $ano
+        ]);
+    }
+
+    public function listar()
+    {
+        $sql = "SELECT * FROM livros";
+
+        $stmt = $this->pdo->query($sql);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function buscarPorId($id)
+    {
+        $sql = "SELECT * FROM livros
+                WHERE id = :id";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->execute([
+            ':id' => $id
+        ]);
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function editar($id, $titulo, $autor, $ano)
+    {
+        $sql = "UPDATE livros
+                SET titulo = :titulo,
+                    autor = :autor,
+                    ano = :ano
+                WHERE id = :id";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        return $stmt->execute([
+            ':id' => $id,
+            ':titulo' => $titulo,
+            ':autor' => $autor,
+            ':ano' => $ano
+        ]);
+    }
+
+    public function excluir($id)
+    {
+        $sql = "DELETE FROM livros
+                WHERE id = :id";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        return $stmt->execute([
+            ':id' => $id
         ]);
     }
 }
